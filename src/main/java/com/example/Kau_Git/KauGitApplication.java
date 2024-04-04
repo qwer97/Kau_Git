75b2d90763a5f6723e5d86b060c10e2212ac53f8
qwer97 <mcrohmh@naver.com>
2024-04-04 19:02:47 +0900

    first

---
+++ src/main/java/com/example/Kau_Git/KauGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.Kau_Git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KauGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KauGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Kau_Git/KauGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Kau_Git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KauGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
