95256352de0c455f4e0710b3072dc0ff51590bed
qwer97 <mcrohmh@naver.com>
2024-06-09 18:46:53 +0900

    new api - 완성

--- src/main/java/com/example/Kau_Git/Service/AreaCodeService.java
+++ src/main/java/com/example/Kau_Git/Service/AreaCodeService.java
@@ -1,5 +1,6 @@
 package com.example.Kau_Git.Service;
 
+
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
 import net.minidev.json.parser.JSONParser;
@@ -10,35 +11,30 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
 @Service
-public class KeywordSearchService {
-
+public class AreaCodeService {
     private final RestTemplate rt;
-    private final AreaCodeService as;
 
     @Value("${api.key}")
     private String apiKey;
 
-    public KeywordSearchService(RestTemplate rt, AreaCodeService as) {
+    public AreaCodeService(RestTemplate rt){
         this.rt = rt;
-        this.as = as;
     }
 
-    private static final String BASE_URL = "https://apis.data.go.kr/B551011/EngService1/searchKeyword1";
+    private static final String BASE_URL ="https://apis.data.go.kr/B551011/EngService1/areaCode1";
 
-    public List<JSONObject> getInfo(int areaCode, int sigunguCode, String keyword) {
-
+    public Map<String, String> getInfo(int areaCode) {
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(BASE_URL)
+                .queryParam("numOfRows", 100) // 총 몇개가 존재하는가?
+                .queryParam("pageNo", 1)
                 .queryParam("MobileOS", "etc")
+                .queryParam("areaCode",areaCode) // 1~8, 31~39
                 .queryParam("MobileApp", "hanzoom")
-                .queryParam("keyword", keyword) // 키워드 추가
-                .queryParam("areaCode", areaCode) // 지역 코드 추가
-                .queryParam("sigunguCode", sigunguCode) // 시군구 코드 추가
-                .queryParam("serviceKey", apiKey); // 서비스 키 추가
+                .queryParam("serviceKey", apiKey);
 
         String uriString = builder.build().encode().toUriString().replace("+", "%2B");
         uriString += "&_type=json";
@@ -47,7 +43,7 @@ public class KeywordSearchService {
 
         String response = rt.getForObject(uri, String.class);
 
-        List<JSONObject> keywordlist = new ArrayList<>();
+        Map<String, String> areaCodeMap = new HashMap<>();
 
         try {
             JSONParser parser = new JSONParser(JSONParser.MODE_JSON_SIMPLE);
@@ -60,28 +56,16 @@ public class KeywordSearchService {
 
             for (Object item : itemArray) {
                 JSONObject itemObj = (JSONObject) item;
-                String addr1 = (String) itemObj.get("addr1");
-                String firstImage = (String) itemObj.get("firstimage");
-                String mapx = (String) itemObj.get("mapx");
-                String mapy = (String) itemObj.get("mapy");
-
-                JSONObject locationInfo = new JSONObject();
-                locationInfo.put("addr1", addr1);
-                locationInfo.put("firstimage", firstImage);
-                locationInfo.put("mapx", mapx);
-                locationInfo.put("mapy", mapy);
-
-                keywordlist.add(locationInfo);
+                String code = String.valueOf(itemObj.get("code"));
+                String name = (String) itemObj.get("name");
+                areaCodeMap.put(code,name);
             }
         } catch (ParseException e) {
             e.printStackTrace();
         }
-
-        return keywordlist;
+        return areaCodeMap;
     }
-
-    public Map<String, String> getAreaInfo(int areaCode) {
-        return as.getInfo(areaCode);
-    }
-
 }
+
+
+
--- src/test/java/com/example/Kau_Git/Service/AreaTest.java
+++ src/test/java/com/example/Kau_Git/Service/AreaTest.java
@@ -1,38 +1,32 @@
 package com.example.Kau_Git.Service;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
 import java.util.Scanner;
 
 @SpringBootTest
-public class KeyWordTest {
-
+public class AreaTest {
     @Autowired
     private RestTemplateBuilder rb;
 
     @Autowired
-    private KeywordSearchService ks;
+    private AreaCodeService as;
 
     @Test
     public void ttest(){
         RestTemplate rt = rb.build();
         Scanner s = new Scanner(System.in);
-        var info = ks.getInfo(1,21,"pharmacy");
 
-        for (var in : info) {
-            System.out.println("주소: " + in.get("addr1"));
-            System.out.println("이미지 URL: " + in.get("firstimage"));
-            System.out.println("위도: " + in.get("mapx"));
-            System.out.println("경도: " + in.get("mapy"));
-            System.out.println("---------------------------");
+        var info = as.getInfo(1);
 
+        for(Map.Entry<String, String> entry : info.entrySet()){
+            System.out.println("코드: " + entry.getKey() + ", 지역이름 " + entry.getValue());
         }
-
-
     }
-
 }
